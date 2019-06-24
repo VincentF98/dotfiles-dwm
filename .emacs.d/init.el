5885ca072f409d208838dc3b6e6e08166442b248
Vincent Fischer <fischervincent98@gmail.com>
2019-06-24 16:22:34 +0200

    updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,7 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auctex)))
+ '(package-selected-packages (quote (helm cdlatex evil-magit auctex)))
  '(safe-local-variable-values (quote ((TeX-master . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
